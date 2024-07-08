e6d5e6471dc07e77e2f5761919c73de3c4c46127
yasua006 <156091445+yasua006@users.noreply.github.com>
2024-07-08 17:06:45 +0200

    less whitespace

--- age calculator/script.js
+++ age calculator/script.js
@@ -1,22 +1,18 @@
 // Get calculated Age
 const getDOB = () => {
-
 	// Getting input from html input element
 	let data = document.getElementById("inputDob").value;
-
 	// Convert input data to usable format
 	// as day,month and year
 	let dob = new Date(data);
 	let day = dob.getDate();
 	let month = dob.getMonth();
 	let year = dob.getFullYear();
-
 	// Getting current date and calculating the difference
 	let now = new Date(document.getElementById("cdate").value);
 	let yearDiff = now.getFullYear() - year;
 	let monthDiff = now.getMonth() - month;
 	let dateDiff = now.getDate() - day;
-
 	// Calculating the Age
 	try {
 	if (yearDiff < 0) console.log("Invalid date entered. Please enter a valid date of birth.");
@@ -34,7 +30,6 @@ const getDOB = () => {
 		dateDiff = 30 + dateDiff;
 		monthDiff -= 1;
 	}
-
 	// Show calculated age as output
 	// and invalid if wrong input is given
 	if (yearDiff < 0)
@@ -45,7 +40,6 @@ const getDOB = () => {
 	console.error(error)
 }
 }
-
 const formatted = (date = new Date()) => {
 	return [
 		date.getFullYear(),
@@ -53,17 +47,14 @@ const formatted = (date = new Date()) => {
 		short(date.getDate()),
 	].join("-");
 }
-
 // Function to provide default date value
 const currentDate = () => {
 	console.log(formatted());
 	let d = document.getElementById("cdate");
 	d.value = formatted();
 }
-
 const short = (num) => {
 	return num.toString().padStart(2, "0");
 }
-
 // Calling current date function to set default date value
 currentDate();
